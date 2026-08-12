c80e0905bc34330e79c711c7c381c9d660cdba2b
siddharth dangi <134779995+siddharthd422@users.noreply.github.com>
2026-08-12 12:46:21 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,7 +42,13 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    // res.cookie("token", token)
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    })
 
 
     res.status(201).json({
@@ -88,7 +94,13 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    // res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    })
+    
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
