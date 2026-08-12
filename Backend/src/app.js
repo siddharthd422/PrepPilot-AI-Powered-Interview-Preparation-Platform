de3d294bf35f411dfd307a576ed4ec30aa70d44a
siddharth dangi <134779995+siddharthd422@users.noreply.github.com>
2026-08-12 12:24:51 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: https://preppilot-ai-ljq3.onrender.com,
+    origin: "https://preppilot-ai-ljq3.onrender.com",
     credentials: true
 }))
 
